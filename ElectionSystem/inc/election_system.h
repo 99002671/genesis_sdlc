088312d0f8a4355007e363c8ff2fdeb4e73bba71
99002671 <71481406+99002671@users.noreply.github.com>
2020-09-27 19:57:55 +0530

    Update election_system.h

--- ElectionSystem/inc/election_system.h
+++ ElectionSystem/inc/election_system.h
@@ -1,8 +1,8 @@
 #ifndef ELECTION_SYSTEM_H_INCLUDED
 #define ELECTION_SYSTEM_H_INCLUDED
 
-void castVote();
-void votesCount();
-void getLeadingCandidate();
+int castVote(int);
+int votesCount(int);
+int getLeadingCandidate(int);
 
 #endif // ELECTION_SYSTEM_H_INCLUDED
